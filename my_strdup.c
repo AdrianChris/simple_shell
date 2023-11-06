55759b28a4a78064c7ff5e726126625b3b55e308
AdrianChris <chrismadegwa4@gmail.com>
2023-11-07 00:04:41 +0300

    Updated

--- my_strdup.c
+++ my_strdup.c
@@ -11,26 +11,28 @@
 char *my_strdup(const char *str)
 {
 	char *ptr;
-	int i, len = 0;
+	size_t len = 0, i;
 
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	while (*str != '\0')
+
+		while (str[len] != '\0')
 	{
 		len++;
-		str++;
 	}
-	str = str - len;
-	ptr = malloc(sizeof(char) * (len + 1));
+	ptr = (char *)malloc((len + 1) * sizeof(char));
+
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
+
 	for (i = 0; i <= len; i++)
 	{
 		ptr[i] = str[i];
 	}
+
 	return (ptr);
 }
